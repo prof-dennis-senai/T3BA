f048a6a96cbb29aca79a86d183870f0547475e51
Dennis Rocha <dennis0201@gmail.com>
2026-03-26 22:07:16 -0300

    aula 3 commit 4

--- SafeIndustry/app_empresa/models.py
+++ SafeIndustry/app_empresa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class EmpresaModel(models.Model):
+    nome = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=14)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nome
--- SafeIndustry/app_empresa/views.py
+++ SafeIndustry/app_empresa/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
+from app_empresa.models import EmpresaModel
 
 # Create your views here.
 def listar_empresas(request):
-    return render(request, 'app_empresa/pages/listar_empresas.html')
+    # empresas1 = EmpresaModel(
+    #     nome='Luzia e Sônia Transportes Ltda',
+    #     cnpj='47568553000109',
+    #     email='FqJ4k@example.com',
+    #     telefone='(18) 3724-8390'
+    # )
+
+    # empresas2 = EmpresaModel(
+    #     nome='Otávio e Tânia Locações de Automóveis Ltda',
+    #     cnpj='70966594000175',
+    #     email='producao@otavioetanialocacoesdeautomoveisltda.com.br',
+    #     telefone='(11) 3748-2999'
+    # )
+
+    # empresas3 = EmpresaModel(
+    #     nome='Almeida montadora Ltda',
+    #     cnpj='47568953000109',
+    #     email='info@almeidamontadora.com',
+    #     telefone='(18) 3724-9631'
+    # )
+
+    # empresas4 = EmpresaModel(
+    #     nome='Santos e Silva Transportes Ltda',
+    #     cnpj='42368553000109',
+    #     email='santos@gmail.com',
+    #     telefone='(18) 3724-8390'
+    # )
+
+    # empresas1.save()
+    # empresas2.save()
+    # empresas3.save()
+    # empresas4.save()
+
+    dados_empresas = EmpresaModel.objects.all()
+    
+    return render(request, 'app_empresa/pages/listar_empresas.html', {'empresas': dados_empresas})
