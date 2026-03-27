796cd48e9f00e06cb6b5af5d78e378da503615e9
Dennis Rocha <dennis0201@gmail.com>
2026-03-26 21:21:48 -0300

    aula 2 commit 2

--- SafeIndustry/safe_industry/settings.py
+++ SafeIndustry/safe_industry/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #My Apps
     'app_blog',
+    'app_empresa',
 ]
 
 MIDDLEWARE = [
--- SafeIndustry/safe_industry/urls.py
+++ SafeIndustry/safe_industry/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app_blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('sobre/', views.sobre, name='sobre'),
+    path('empresa/', include('app_empresa.urls')),
 ]
